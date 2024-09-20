b5db2f964180841fc2fbcf91223b98c9c4080d0b
singularnist <121468102+singularnist@users.noreply.github.com>
2024-09-20 16:27:12 +0300

    Delete src directory

--- src/config/apiconf.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Api_Conf():
-    
-    API_URL = 'http://mreport.enet.vn.ua'
-    API_PORT = '6000'
-    API_GRAF = "/api/graf"
-    API_DASHBORD = '/api/dashbord'
--- src/config/colorconf.py
+++
@@ -1,8 +0,0 @@
-
-class Color_Conf():
-    WHITE = '#fafafc'
-    bg_color = '#282a36'
-    WARNING = '#f5e905'
-    CRITICAL = '#f50505'
-    LIGHT_GREEN = "#05f51d"
-    LIGHT_BLUE = '#05f5f1'
